86c9ec9713706733ae980ac623b715d9b97aaff7
권세훈 <Kwonsaehoon@sehoon.local>
2021-07-02 16:11:34 +0900

    [chore] test.ts  수정

--- dist/loaders/db.js
+++ dist/loaders/db.js
@@ -13,7 +13,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
-const test_1 = __importDefault(require("../models/test"));
 const config_1 = __importDefault(require("../config"));
 const connectDB = () => __awaiter(void 0, void 0, void 0, function* () {
     try {
@@ -23,21 +22,21 @@ const connectDB = () => __awaiter(void 0, void 0, void 0, function* () {
             useUnifiedTopology: true,
         });
         console.log("Mongoose Connected ...");
-        test_1.default.createCollection().then(function (collection) {
-            console.log('Banner Collection is created!');
-        });
-        test_1.default.insertMany([
-            {
-                "hello": "야 들어가냐?",
-                "image": "https://catchmeserver.s3.us-east-2.amazonaws.com/image.png"
-            },
-            {
-                "hello": "들어간거 맞지??",
-                "image": "https://catchmeserver.s3.us-east-2.amazonaws.com/image.png"
-            },
-        ]).then(function (collection) {
-            console.log("Data Insert Success.");
-        });
+        // Test.createCollection().then(function (collection) {
+        //     console.log('Banner Collection is created!');
+        // });
+        // Test.insertMany([
+        //     {
+        //         "hello": "야 들어가냐?",
+        //         "image": "https://catchmeserver.s3.us-east-2.amazonaws.com/image.png"
+        //     },
+        //     {
+        //         "hello": "들어간거 맞지??",
+        //         "image": "https://catchmeserver.s3.us-east-2.amazonaws.com/image.png"
+        //     },
+        // ]).then(function (collection) {
+        //     console.log("Data Insert Success.");
+        // });
     }
     catch (err) {
         console.error(err.message);
