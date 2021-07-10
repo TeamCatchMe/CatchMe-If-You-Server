4a6a7f5c6250c516c022e272758e27c0106f3738
권세훈 <Kwonsaehoon@sehoon.local>
2021-07-11 03:56:25 +0900

    [fix] character API data 수정

--- src/api/character.ts
+++ src/api/character.ts
@@ -13,34 +13,44 @@ const router = Router();
  *  @access Public
  */
 router.get("/", auth ,async (req: Request, res: Response) => {
-  try {//4,6
+  try {
   
     const time = moment();
     var month = time.format('MM');
 
-    const activities = await Activity
-      .find({ user_id : req.body.user.id}) //, activityDateMonth : month
+    const character = await Character.findOne({ user_id : req.body.user.id, characterIndex : 6 }).select({ user_id : 0, _id : 0 });
+    const { characterBirth, characterName, characterLevel, characterIndex } = character;
+
+    const characterActivities = await Activity
+      .find({ user_id : req.body.user.id, characterIndex : characterIndex})
       .select({ user_id : 0, _id : 0 });
-    
-    // const catching = characters.find( element => activityDate.slice(4,6) == month );
 
-    console.log(activities);
+    const allActivities = await Activity.find({user_id : req.body.user.id}).countDocuments();
 
-    if ( activities.length == 0 ) {
+    console.log(characterActivities);
+
+    if ( characterActivities.length == 0 ) {
       return res.status(400).json({
         "status" : 400,
         "success" : false,
-        "message" : month + "월의 게시글이 존재 하지 않습니다.",
+        "message" : characterName + "의 " + month + "월 게시글이 존재 하지 않습니다.",
         "data" : null
       });
     };
-
+    
+    // 캐치지수 : 전체 활동중 해당 캐릭터의 활동 비율
+    const catchRate = characterActivities.length / allActivities * 100
+    
     res.json({
       "status" : 200,
       "success" : true,
-      "message" : "월별 게시글 데이터 불러오기 성공",
+      "message" : "캐릭터 상세정보 불러오기 성공",
       "data": {
-        activities
+        characterBirth : characterBirth,
+        characterName : characterName,
+        characterLevel : characterLevel,
+        catchRate : catchRate,
+        activities : characterActivities
       }
     });
     
@@ -56,5 +66,5 @@ router.get("/", auth ,async (req: Request, res: Response) => {
   }
 });
 
-console.log("report API 불러오기 성공");
+console.log("character API 불러오기 성공");
 module.exports = router;
