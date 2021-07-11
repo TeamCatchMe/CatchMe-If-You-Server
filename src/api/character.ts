4e14d81de9331e2892fbfc64c0a8fc5aa11f848e
권세훈 <Kwonsaehoon@sehoon.local>
2021-07-11 23:59:43 +0900

    [fix] report, character API 수정

--- src/api/character.ts
+++ src/api/character.ts
@@ -1,7 +1,6 @@
 // 작업중
 import { Router, Request, Response } from "express";
 import auth from "../middleware/auth"
-import Activity from "../models/Activity";
 import Character from "../models/Character";
 
 const moment = require('moment');
@@ -14,47 +13,52 @@ const router = Router();
  */
 router.get("/", auth ,async (req: Request, res: Response) => {
   try {
-  
+    var allActivitiesCount = 0;
     const time = moment();
     var month = time.format('MM');
+    const { characterIndex } = req.body;
 
-    const character = await Character.findOne({ user_id : req.body.user.id, characterIndex : 6 }).select({ user_id : 0, _id : 0 });
-    const { characterBirth, characterName, characterLevel, characterIndex } = character;
+    // req.body 의 characterIndex를 가지는 캐릭터의 데이터를 가져옵니다. (객체)
+    const character = await Character
+    .findOne({ user_id : req.body.user.id, characterIndex : characterIndex})
+    .select({ user_id : 0, _id : 0 });
 
-    const characterActivities = await Activity
-      .find({ user_id : req.body.user.id, characterIndex : characterIndex})
-      .select({ user_id : 0, _id : 0 });
-
-    const allActivities = await Activity.find({user_id : req.body.user.id}).countDocuments();
-
-    console.log(characterActivities);
-
-    if ( characterActivities.length == 0 ) {
+    if ( character['activity'].length == 0 ) {
       return res.status(400).json({
-        "status" : 400,
-        "success" : false,
-        "message" : characterName + "의 " + month + "월 게시글이 존재 하지 않습니다.",
+        "status" : 200,
+        "success" : true,
+        "message" : "통신성공, 당월 게시글이 존재하지 않습니다.",
         "data" : null
       });
     };
+
+    // 전체 캐릭터의 전체 게시글을 가져옵니다.
+    const allActivities = await Character
+    .find({user_id : req.body.user.id}, { activity : true })
+    .sort({ activityYear : -1, activityMonth : -1 });
+    
+    // 전체 캐릭터가 쓴 게시글의 총 개수를 구합니다.
+    for (var i = 0; i < allActivities.length; i++) {
+      allActivitiesCount += allActivities[i]["activity"].length
+    };
+    // 캐릭터가 쓴 게시글의 총 개수를 구합니다.
+    const characterActivitiesCount = character['activity'].length;
     
     // 캐치지수 : 전체 활동중 해당 캐릭터의 활동 비율
-    const catchRate = characterActivities.length / allActivities * 100
+    const catchRate = Math.floor(characterActivitiesCount / allActivitiesCount * 100);
     
     res.json({
       "status" : 200,
       "success" : true,
       "message" : "캐릭터 상세정보 불러오기 성공",
       "data": {
-        characterBirth : characterBirth,
-        characterName : characterName,
-        characterLevel : characterLevel,
-        catchRate : catchRate,
-        activities : characterActivities
+        character,
+        characterActivitiesCount,
+        catchRate
       }
     });
-    
-    console.log("월별 게시글 데이터 불러오기 성공");
+
+    console.log("캐릭터 상세정보 불러오기 성공");
 
   } catch (error) {
     console.error(error.message);
