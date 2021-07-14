3282dfec8c6bb9871adfc9917a7fbfaf554805e6
realwhyjay <brandon94_@naver.com>
2021-07-15 01:40:25 +0900

    [feat] 비밀번호 변경 API 완성

--- src/api/setting.ts
+++ src/api/setting.ts
@@ -105,4 +105,57 @@ router.post("/passwordcheck", auth, async (req, res) => {
   }
 });
 
+/**
+ *  @route Post setting/password
+ *  @desc edit password
+ *  @access Public
+ */
+router.post("/password", auth, async (req, res) => {
+  try {
+    console.log("[/setting/password] 비밀번호 변경 시도");
+
+    var password = req.body.password;
+    console.log("입력받은 pw", password);
+
+    let user = await UserData.findOne({ _id: req.body.user.id });
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    // 비밀번호 일치하는 경우
+    if (isMatch) {
+      console.log(
+        "[/setting/passwordcheck] 비밀번호 변경 실패 - 현재 비밀번호와 똑같음"
+      );
+      res.status(200).json({
+        status: 200,
+        success: false,
+        message: "현재 비밀번호와 똑같습니다.",
+      });
+    }
+
+    // password 암호화
+    const salt = await bcrypt.genSalt(10);
+    password = await bcrypt.hash(password, salt);
+
+    await UserData.findOneAndUpdate(
+      { _id: req.body.user.id },
+      { password: password }
+    );
+
+    console.log("[/setting/password] 비밀번호 변경 성공");
+    return res.status(200).json({
+      status: 200,
+      success: true,
+      message: "비밀번호 변경 성공",
+    });
+  } catch (err) {
+    console.log("[/setting/password] 비밀번호 변경 실패 - 서버 내부 오류");
+    console.error(err.message);
+    res.status(500).json({
+      status: 500,
+      success: false,
+      message: "서버 내부 오류",
+    });
+  }
+});
+
 module.exports = router;
