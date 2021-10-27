77ccdc5e627cfba842bacf976900f6b577a78ae4
realwhyjay <brandon94_@naver.com>
2021-10-28 02:34:30 +0900

    activity db 수정 및 activity 코드 전면 수정

--- src/api/dbtest.ts
+++ src/api/dbtest.ts
@@ -54,17 +54,23 @@ router.post(
       const activityCount = character["activityCount"];
 
       // 만약, Activity가 비어있다면 activityIndex를 1로 설정해줌
+      // 그렇지 않은 경우에는 마지막 index+1로 설정한다.
+      const lastActivity = await Activity.findOne({
+        user_id: req.body.user.id,
+        characterIndex: characterIndex,
+      }).sort({ activityIndex: -1 });
+
       if (activityCount == 0) {
         activityIndex = 1;
       } else {
-        activityIndex = activityCount + 1;
+        activityIndex = lastActivity.activityIndex + 1;
       }
 
       var activityImage = "";
       var activityImageName = "";
       if (req.file) {
-        activityImage = req.file.location;
-        activityImageName = req.file.key;
+        activityImage = req.file.transforms[0]["location"];
+        activityImageName = req.file.transforms[0]["key"];
       }
 
       const activityAdded = new Activity({
@@ -81,8 +87,10 @@ router.post(
         activityImageName: activityImageName,
       });
 
+      // 해당 캐릭터의 activity 개수
       const countAfterUpdate = activityCount + 1;
 
+      // 새로 생성한 activity의 id 값을 character에 추가하고, activity의 개수를 업데이트 해준다.
       await Character.findOneAndUpdate(
         { user_id: req.body.user.id, characterIndex: characterIndex },
         {
@@ -97,6 +105,37 @@ router.post(
       console.log("[추가된 데이터] \n", activityAdded);
 
       await activityAdded.save();
+
+      // 활동 기록 수를 확인하여 캐릭터 레벨업
+      if (countAfterUpdate == 11) {
+        await Character.findOneAndUpdate(
+          { user_id: req.body.user.id, characterIndex: characterIndex },
+          {
+            characterLevel: 2,
+          }
+        );
+        console.log(logger.ACTIVITY_LEVEL_UP_2, "[", logTime, "]");
+        return res.status(222).json({
+          status: 222,
+          success: true,
+          message:
+            "게시물 등록 성공 && 캐릭터 레벨 업!!! 끼얏호오오~~ 레벨업이다!!",
+        });
+      } else if (countAfterUpdate == 31) {
+        await Character.findOneAndUpdate(
+          { user_id: req.body.user.id, characterIndex: characterIndex },
+          {
+            characterLevel: 3,
+          }
+        );
+        console.log(logger.ACTIVITY_LEVEL_UP_3, "[", logTime, "]");
+        return res.status(222).json({
+          status: 222,
+          success: true,
+          message:
+            "게시물 등록 성공 && 캐릭터 레벨 업!!! 끼얏호오오~~ 레벨업이다!!",
+        });
+      }
 
       console.log(logger.OK_ACTIVITY_NEW, "[", logTime, "]");
       return res.status(200).json({
@@ -115,6 +154,248 @@ router.post(
     }
   }
 );
+
+/**
+ *  @route Post activity/edit
+ *  @desc edit new activity
+ *  @access Public
+ */
+router.post("/edit", upload.single("activityImage"), auth, async (req, res) => {
+  const time = moment();
+  var logTime = time.format("HH:mm:ss");
+
+  const {
+    activityContent,
+    activityYear,
+    activityMonth,
+    activityDay,
+    characterIndex,
+    activityIndex,
+  } = req.body;
+
+  try {
+    console.log(logger.TRY_ACTIVITY_EDIT, "[", logTime, "]");
+
+    // 캐릭터 인덱스에 해당하는 액티비티 불러옴
+    const objectActivity = await Activity.findOne({
+      user_id: req.body.user.id,
+      characterIndex: characterIndex,
+      activityIndex: activityIndex,
+    });
+
+    // 이미지를 새로 업로드하지 않는 경우에는 기존 이미지 값을 가져온다.
+    var activityImage = objectActivity["activityImage"];
+    var activityImageName = objectActivity["activityImageName"];
+
+    if (req.file) {
+      // 새로 이미지를 업데이트 하는경우, 기존 이미지의 이름을 찾아 imageKey에 저장한다
+      const imageKey = objectActivity["activityImageName"];
+
+      // 서버에서 해당 이미지를 삭제한다.
+      s3.deleteObject(
+        {
+          Bucket: "catchmeserver", // 사용자 버켓 이름
+          Key: imageKey, // 버켓 내 경로
+        },
+        (err, data) => {
+          if (err) {
+            throw err;
+          }
+        }
+      );
+
+      // 새로 저장된 이미지의 정보를 가져온다
+      activityImage = req.file.transforms[0]["location"];
+      activityImageName = req.file.transforms[0]["key"];
+    }
+
+    // 수정할 값들을 바탕으로 데이터를 수정해준다.
+    await Activity.findOneAndUpdate(
+      {
+        user_id: req.body.user.id,
+        characterIndex: characterIndex,
+        activityIndex: activityIndex,
+      },
+      {
+        activityContent: activityContent,
+        activityImage: activityImage,
+        activityYear: activityYear,
+        activityMonth: activityMonth,
+        activityDay: activityDay,
+        activityImageName: activityImageName,
+      }
+    );
+
+    console.log("[수정된 데이터] \n", {
+      user_id: req.body.user.id,
+      activityIndex: objectActivity.activityIndex,
+      activityContent: activityContent,
+      activityImage: activityImage,
+      activityYear: activityYear,
+      activityMonth: activityMonth,
+      activityDay: activityDay,
+      characterIndex: objectActivity.characterIndex,
+      activityImageName: activityImageName,
+    });
+
+    console.log(logger.OK_ACTIVITY_EDIT, "[", logTime, "]");
+    return res.status(200).json({
+      status: 200,
+      success: true,
+      message: "게시글 수정 성공",
+    });
+  } catch (err) {
+    console.log(logger.FAIL_ACTIVITY_EDIT, "[", logTime, "]");
+    console.error(err.message);
+    res.status(500).json({
+      status: 500,
+      success: false,
+      message: "서버 내부 오류",
+    });
+  }
+});
+
+/**
+ *  @route Post activity/delete
+ *  @desc delete  activity
+ *  @access Public
+ */
+router.post("/delete", auth, async (req, res) => {
+  const time = moment();
+  var logTime = time.format("HH:mm:ss");
+  var activityUpdateTime = "0";
+  const { characterIndex, activityIndex } = req.body;
+
+  try {
+    console.log(logger.TRY_ACTIVITY_DELETE, "[", logTime, "]");
+
+    // 수정할 값들을 바탕으로 데이터를 삭제한다.
+    const deletedActivity = await Activity.findOneAndDelete({
+      user_id: req.body.user.id,
+      characterIndex: characterIndex,
+      activityIndex: activityIndex,
+    });
+
+    console.log(deletedActivity);
+
+    // Character 컬렉션에 추가하기 위해 새로 activity를 find 해준다.
+    var character = await Character.findOne({
+      user_id: req.body.user.id,
+      characterIndex: characterIndex,
+    });
+
+    let activityCount = character.activityCount;
+    let edittedActivityCount;
+
+    // 캐릭터의 activityCount와 activityUpdateTime을 업데이트 해주기위한 작업
+    if (activityCount == 1) {
+      /*  
+      activity가 하나일 경우에는, 
+      activityCount를 0으로
+      activityUpdateTime을 캐릭터 생성 시간으로 수정한다.
+      */
+      const forEdit = await Character.findOne({
+        user_id: req.body.user.id,
+        characterIndex: characterIndex,
+      });
+
+      activityUpdateTime = forEdit["characterBirth"];
+      edittedActivityCount = 0;
+    } else {
+      /*  
+      activity가 두 개 이상일 경우에는, 
+      activityCount를 하나 뺀 수으로
+      activityUpdateTime을 삭제하고 남은 액티비티의 가장 최근 시간으로 수정한다.
+      */
+      const activityForTime = await Activity.find({
+        user_id: req.body.user.id,
+        characterIndex: characterIndex,
+      }).sort({ recentActivityTime: -1 });
+
+      console.log("activityForTime : ", activityForTime);
+      console.log(
+        "activityForTime[0]['recentActivityTime'] : ",
+        activityForTime[0]["recentActivityTime"]
+      );
+      activityUpdateTime = activityForTime[0]["recentActivityTime"];
+      edittedActivityCount = activityCount - 1;
+    }
+
+    // 앞서 정한 정보들을 바탕으로 Character에 삭제된 activity 데이터를 빼주고,
+    // 마지막 활동 시간과 activityCount를 바꿔준다.
+    await Character.findOneAndUpdate(
+      { user_id: req.body.user.id, characterIndex: characterIndex },
+      {
+        recentActivityTime: activityUpdateTime,
+        activityCount: edittedActivityCount,
+        $pull: { activityId: deletedActivity._id },
+      }
+    );
+
+    if (deletedActivity["activityImageName"] != "") {
+      // 삭제한 이미지의 위치를 imageKey에 저장한다
+      const imageKey = deletedActivity["activityImageName"];
+      // 서버에서 해당 이미지를 삭제한다.
+      s3.deleteObject(
+        {
+          Bucket: "catchmeserver", // 사용자 버켓 이름
+          Key: imageKey, // 버켓 내 경로
+        },
+        (err, data) => {
+          if (err) {
+            throw err;
+          }
+        }
+      );
+    }
+
+    console.log("[삭제된 데이터] \n", deletedActivity);
+
+    // 활동 기록 수를 확인하여 캐릭터 레벨을 돌려준다.
+    if (edittedActivityCount == 10) {
+      await Character.findOneAndUpdate(
+        { user_id: req.body.user.id, characterIndex: characterIndex },
+        {
+          characterLevel: 1,
+        }
+      );
+      console.log(logger.ACTIVITY_LEVEL_DOWN_1, "[", logTime, "]");
+      return res.status(222).json({
+        status: 222,
+        success: true,
+        message: "게시물 삭제 성공 && 캐릭터 레벨 다운... 에휴 쯧쯧...",
+      });
+    } else if (edittedActivityCount == 30) {
+      await Character.findOneAndUpdate(
+        { user_id: req.body.user.id, characterIndex: characterIndex },
+        {
+          characterLevel: 2,
+        }
+      );
+      console.log(logger.ACTIVITY_LEVEL_DOWN_2, "[", logTime, "]");
+      return res.status(222).json({
+        status: 222,
+        success: true,
+        message: "게시물 삭제 성공 && 캐릭터 레벨 다운... 에휴 쯧쯧...",
+      });
+    }
+
+    console.log(logger.OK_ACTIVITY_DELETE, "[", logTime, "]");
+    return res.status(200).json({
+      status: 200,
+      success: true,
+      message: "게시글 삭제 성공",
+    });
+  } catch (err) {
+    console.log(logger.FAIL_ACTIVITY_DELETE, "[", logTime, "]");
+    console.error(err.message);
+    res.status(500).json({
+      status: 500,
+      success: false,
+      message: "서버 내부 오류",
+    });
+  }
+});
 
 /**
  *  @route GET character/:characterIndex
@@ -200,6 +481,7 @@ router.get(
         "catchRate : ",
         catchRate
       );
+
       console.log(
         logger.OK_CHARACTER,
         "<",
